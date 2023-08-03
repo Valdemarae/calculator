4c8c2a094fd537b16a34d3c25612a4bd5f42fb4d
Valdemar <valdemar.kobelis@gmail.com>
2023-08-03 16:56:45 +0300

    Create function that calls desired operation function

--- javascript.js
+++ javascript.js
@@ -14,7 +14,17 @@ function divide (a, b) {
     return a / b;
 }
 
-console.log(add(35, 23));
-console.log(subtract(55, 5));
-console.log(multiply(3, 4));
-console.log(divide(55, 5));
+function operate (firstNumber, operator, secondNumber) {
+    switch (operator) {
+        case '+':
+            return add(firstNumber, secondNumber);
+        case '-':
+            return subtract(firstNumber, secondNumber);
+        case '*':
+            return multiply(firstNumber, secondNumber);
+        case '/':
+            return divide(firstNumber, secondNumber);
+    }
+}
+
+let firstNumber, secondNumber, operator;

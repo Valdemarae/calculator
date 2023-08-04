f70d9eb6edb4757a5d5e26a0580be0cdb16ee7d8
Valdemar <valdemar.kobelis@gmail.com>
2023-08-04 10:18:09 +0300

    Create function that populates the display when buttons are clicked

--- javascript.js
+++ javascript.js
@@ -27,4 +27,45 @@ function operate (firstNumber, operator, secondNumber) {
     }
 }
 
+function displayButtonValue () {
+    for (button of buttons) {
+        if (button.classList.contains('1')) {
+            button.addEventListener('click', () => displayValues.textContent += '1');
+        } else if (button.classList.contains('2')) {
+            button.addEventListener('click', () => displayValues.textContent += '2');
+        } else if (button.classList.contains('3')) {
+            button.addEventListener('click', () => displayValues.textContent += '3');
+        } else if (button.classList.contains('4')) {
+            button.addEventListener('click', () => displayValues.textContent += '4');
+        } else if (button.classList.contains('5')) {
+            button.addEventListener('click', () => displayValues.textContent += '5');
+        } else if (button.classList.contains('6')) {
+            button.addEventListener('click', () => displayValues.textContent += '6');
+        } else if (button.classList.contains('7')) {
+            button.addEventListener('click', () => displayValues.textContent += '7');
+        } else if (button.classList.contains('8')) {
+            button.addEventListener('click', () => displayValues.textContent += '8');
+        } else if (button.classList.contains('9')) {
+            button.addEventListener('click', () => displayValues.textContent += '9');
+        } else if (button.classList.contains('divi')) {
+            button.addEventListener('click', () => displayValues.textContent += '÷');
+        } else if (button.classList.contains('multi')) {
+            button.addEventListener('click', () => displayValues.textContent += '×');
+        } else if (button.classList.contains('addd')) {
+            button.addEventListener('click', () => displayValues.textContent += '+');
+        } else if (button.classList.contains('subtra')) {
+            button.addEventListener('click', () => displayValues.textContent += '-');
+        } else if (button.classList.contains('dot')) {
+            button.addEventListener('click', () => displayValues.textContent += '.');
+        } else if (button.classList.contains('0')) {
+            button.addEventListener('click', () => displayValues.textContent += '0');
+        }
+        displayContent = displayValues.textContent;
+    }
+}
+
 let firstNumber, secondNumber, operator;
+const buttons = document.querySelectorAll('button');
+const displayValues = document.querySelector('.displayValues');
+let displayContent = "";
+displayButtonValue();
